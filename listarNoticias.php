8e89323846b50653e77be70cdcae300df642b7df
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-03-19 08:35:00 -0300

    Códigos fonte de aulas
    Códigos fonte de aulas

---
+++ listarNoticias.php
@@ -0,0 +1,19 @@
+<?php
+	include ("Noticia.php");
+	$listarNoticia = new Noticia();
+	$valor = $listarNoticia->buscarTodos();
+	
+	print_r($valor);
+?>	
+<table border="1">
+	<th>Item</th>
+	<th>Título</th>
+	<th>Autor</th>
+	<th>Ação</th>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+</table>
